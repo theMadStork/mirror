58f93e0ee0bad527e42722a0ab79860bc3da8e34
lizzie <lizzie@eden-emu.dev>
2025-12-06 03:13:11 +0000

    fucking license

--- src/common/fs/file.h
+++ src/common/fs/file.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: Copyright 2021 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
